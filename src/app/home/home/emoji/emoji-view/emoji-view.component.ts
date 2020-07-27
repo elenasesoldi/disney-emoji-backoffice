9111ab0bbc98d09df6f15681acc1ad2f971c6675
elena <elenasesoldi@gmail.com>
2020-07-28 01:04:19 +0200

    aggiunto bottone elimina emoji

--- src/app/home/home/emoji/emoji-view/emoji-view.component.ts
+++ src/app/home/home/emoji/emoji-view/emoji-view.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Emoji } from '../../../../../model/emoji.model';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { EditEmojiComponent } from '../edit-emoji/edit-emoji.component';
+import { EmojiService } from '../../../../../service/emoji.service';
 
 @Component({
   selector: 'app-emoji-view',
@@ -13,7 +14,7 @@ export class EmojiViewComponent implements OnInit {
   @Input()
   emoji: Emoji;
 
-  constructor(private modalService: NgbModal) { }
+  constructor(private modalService: NgbModal, private emojiService: EmojiService) { }
 
   ngOnInit() {
   }
@@ -22,6 +23,9 @@ export class EmojiViewComponent implements OnInit {
     const modal = this.modalService.open(EditEmojiComponent, { centered: true, size: 'lg' });
     modal.componentInstance.caricaEmoji(this.emoji.id);
 
+  }
+
+  elimina(): void {
   }
 
 }
